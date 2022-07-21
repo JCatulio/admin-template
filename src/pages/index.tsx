c033b4e6761a3ec4943e721f5909df04f1752ea9
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-07-21 20:44:45 -0300

    Autenticacao

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/templates/Layout";
+
+export default function Home() {
+  return (
+    <Layout
+      titulo="Página Inicial"
+      subtitulo="Estamos construindo um template admin"
+    >
+      <h3>Conteúdo</h3>
+    </Layout>
+  );
+}
